f4dac8514c90c1e2799b5f5cd4bc4414f1178fa5
KomiszarJanos <csze2komjan@vasvari.org>
2025-01-20 09:49:19 +0100

    a filmModel kész

---
+++ models/film.js
@@ -0,0 +1,14 @@
+const mongoose= require('mongoose');
+const filmscheema= new mongoose.scheema(
+    {   id: {type:Number, required:true},
+        name: {type:String, required:true},
+        description: {type:String, required:true},
+        image:{type:String, required:true},
+        rating:{type:Number, required: true},
+        genres: {type:[String], enum:['Crime', 'Drama', 'Action', 'Comedy'],
+             required: true},
+        inTheaters:{type:Boolean, required:true}
+
+    }
+);
+module.exports=mongoose.model("Films", filmscheema);
